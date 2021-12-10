74fe3df37f54ec05c65ca2045fa4c480ce8089cb
Thomas <82314824+tlaharotte@users.noreply.github.com>
2021-12-10 11:02:34 +0100

    Add files via upload

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Controller;
-
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Knp\Component\Pager\PaginatorInterface;
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Controller;
-
 use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
